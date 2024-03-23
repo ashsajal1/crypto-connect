cf428a5207c2fb88bb51cf0c3b9aa01ff936d697
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-23 17:29:44 +0800

    update handleConnect

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,10 +7,38 @@ import {
     Spacer,
     useColorMode,
 } from '@chakra-ui/react';
-import handleConnectWallet from '../ilb/connect-wallet';
+import { useDispatch, useSelector } from 'react-redux';
+import { connectWalletSuccess, connectWalletFailure } from '../features/walletSlice'; // Importing the Redux actions
+import Web3 from 'web3';
 
 const Navbar = () => {
     const { colorMode, toggleColorMode } = useColorMode();
+    const dispatch = useDispatch(); // Get dispatch function from Redux
+    const {accounts, connected} = useSelector(state => state.wallet);
+
+    const handleConnectWallet = async () => {
+        // Check if MetaMask is installed
+        if (window.ethereum) {
+            try {
+                // Request access to the user's MetaMask accounts
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+                // Create a Web3 instance using MetaMask provider
+                const web3 = new Web3(window.ethereum);
+
+                // You can now use 'web3' to interact with the user's wallet
+                // For example, you can fetch the user's accounts
+                const accounts = await web3.eth.getAccounts();
+                dispatch(connectWalletSuccess(accounts));
+                console.log('Connected accounts:', accounts);
+            } catch (error) {
+                dispatch(connectWalletFailure((error as Error).message));
+                console.error('Error connecting to MetaMask:', (error as Error).message);
+            }
+        } else {
+            console.error('MetaMask not detected. Please install MetaMask to connect your wallet.');
+        }
+    };
 
     return (
         <Flex
@@ -24,7 +52,7 @@ const Navbar = () => {
             <Image src={'/icon.jpg'} alt="CryptoConnect Icon" boxSize="48px" mr="4" />
             <Spacer />
             <Box>
-                <Button mr="2" onClick={handleConnectWallet}>Connect Wallet</Button>
+                <Button mr="2" onClick={handleConnectWallet}>Connect Wallet {connected} {accounts}</Button>
                 <Button onClick={toggleColorMode}>
                     {colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
                 </Button>
