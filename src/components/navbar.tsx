f54c51b407f6994a512b1be400f11016d919ef9d
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-23 18:18:17 +0800

    Edit connect button after connected

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -70,7 +70,7 @@ const Navbar = () => {
             <Image src={'/icon.jpg'} alt="CryptoConnect Icon" boxSize="48px" mr="4" />
             <Spacer />
             <Box>
-                <Button mr="2" onClick={handleConnectWallet}>Connect Wallet {connected} {accounts}</Button>
+                <Button mr="2" onClick={handleConnectWallet}> {connected ? "Connected":'Connect Wallet'} {accounts && accounts} </Button>
                 <Button onClick={toggleColorMode}>
                     {colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
                 </Button>
